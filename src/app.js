9f4fcdb50ea870c2f8d4f28ee4b653908c9cdf2d
seon-b <86736916+seon-b@users.noreply.github.com>
2024-12-07 23:34:25 -0500

    Updated app.js

--- src/app.js
+++ src/app.js
@@ -3,11 +3,7 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, "public")));
-
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
+app.use(express.static(path.join(__dirname, "../public")));
 
 app.listen(PORT, () => {
   console.log(`Task Management App listening on port ${PORT}`);
