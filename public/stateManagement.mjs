fca26f26b228a4968762c1e3825ef79af57c6e71
seon-b <86736916+seon-b@users.noreply.github.com>
2024-12-13 20:58:21 -0500

    Modified appState object to track the id of components

--- public/stateManagement.mjs
+++ public/stateManagement.mjs
@@ -1,16 +1,30 @@
 export let appState = {
+  componentIdList: [],
   taskDeadline: "",
-  taskStatus: "active",
   taskName: "",
+  taskStatus: "active",
 };
 
 export function setAppState(state, value) {
-  if (state === "taskDeadline") {
+  if (state === "componentIdListAdd") {
+    let newArray = appState.componentIdList;
+    newArray.push(value);
+    appState = {
+      ...appState,
+      componentIdList: newArray,
+    };
+  } else if (state === "componentIdListRemove") {
+    appState.componentIdList.find((id, index) => {
+      if (id === value) {
+        appState.componentIdList.splice(index, 1);
+      }
+    });
+  } else if (state === "taskDeadline") {
     appState = { ...appState, taskDeadline: value };
-  } else if (state === "taskStatus") {
-    appState = { ...appState, taskStatus: value };
   } else if (state === "taskName") {
     appState = { ...appState, taskName: value };
+  } else if (state === "taskStatus") {
+    appState = { ...appState, taskStatus: value };
   } else {
   }
 }
