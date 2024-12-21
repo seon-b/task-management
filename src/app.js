9cbabf51ac843b5c414a7bc39271e0564d0f4c33
seon-b <86736916+seon-b@users.noreply.github.com>
2024-12-20 20:46:10 -0500

    Installed helmet

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const helmet = require("helmet");
 const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(helmet);
 
 app.listen(PORT, () => {
   console.log(`Task Management App listening on port ${PORT}`);
