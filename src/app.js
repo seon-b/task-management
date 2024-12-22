8152583be1efab8e39e7e45bb102e47ff2ccf97a
seon-b <86736916+seon-b@users.noreply.github.com>
2024-12-21 19:01:29 -0500

    Installed ejs

--- src/app.js
+++ src/app.js
@@ -9,6 +9,8 @@ app.use(express.static(path.join(__dirname, "../public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.set("view engine", "ejs");
+
 app.listen(PORT, () => {
   console.log(`Task Management App listening on port ${PORT}`);
 });
