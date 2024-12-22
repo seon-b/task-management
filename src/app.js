729bf7be95c07df8acf120a206d1107d5d9c5466
seon-b <86736916+seon-b@users.noreply.github.com>
2024-12-21 21:21:30 -0500

    Created index.ejs

--- src/app.js
+++ src/app.js
@@ -5,11 +5,16 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(helmet());
-app.use(express.static(path.join(__dirname, "../public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "../public")));
 
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "../views"));
+
+app.get("/", (req, res, next) => {
+  res.render("index");
+});
 
 app.listen(PORT, () => {
   console.log(`Task Management App listening on port ${PORT}`);
