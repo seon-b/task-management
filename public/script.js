eab3693413dd3add8ca6aaca3eb85d159bc00440
seon-b <86736916+seon-b@users.noreply.github.com>
2024-12-28 12:33:26 -0500

    Created functions to clear form and get user selection

--- public/script.js
+++ public/script.js
@@ -1,21 +1,30 @@
 import { appState, setAppState } from "./stateManagement.mjs";
 import { addComponent, removeComponent } from "./components.mjs";
 
+const completeRadioButton = document.querySelector("#complete");
+const activeRadioButton = document.querySelector("#active");
 const currentTaskComponent = document.querySelector("[data-current-task]");
 const completedTaskComponent = document.querySelector("[data-completed-task]");
 const createTaskButton = document.querySelector(".submitButton");
+const newTaskComponent = document.querySelector("[data-new-task]");
 const taskNameInput = document.querySelector("#taskName");
 const taskDeadlineInput = document.querySelector("#taskDeadline");
 const taskContentInput = document.querySelector("#taskContent");
-const taskStatusInputs = document.querySelectorAll(".taskStatus");
 
 createTaskButton.addEventListener("click", (e) => {
   e.preventDefault();
   addComponent("#taskComponent", newTaskComponent, appState);
+  setAppState("clearForm");
+  clearForm();
 });
 
+function clearForm() {
+  taskContentInput.value = appState.taskContent;
+  taskDeadlineInput.value = appState.taskDeadline;
+  taskNameInput.value = appState.taskContent;
+  activeRadioButton.checked = true;
+}
 function getUserInput(e) {
-  let className = ".";
   let idName = "#";
   let inputLabel = e.target.name;
   let inputValue;
@@ -32,16 +41,21 @@ function getUserInput(e) {
     let element = document.querySelector(idName + inputLabel);
     inputValue = element.value;
     setAppState("taskContent", inputValue);
-  } else if (inputLabel === "taskStatus") {
-    let element = document.querySelector(className + inputLabel);
-    inputValue = element.value;
-    setAppState("taskStatus", inputValue);
+  } else {
+  }
+}
+
+function getUserSelection(e) {
+  if (e.target.id === "complete") {
+    setAppState("taskStatus", e.target.value);
+  } else if (e.target.id === "active") {
+    setAppState("taskStatus", e.target.value);
+  } else {
   }
 }
 
 taskNameInput.addEventListener("change", (e) => getUserInput(e));
 taskDeadlineInput.addEventListener("change", (e) => getUserInput(e));
 taskContentInput.addEventListener("change", (e) => getUserInput(e));
-taskStatusInputs.forEach((input) =>
-  input.addEventListener("change", (e) => getUserInput(e))
-);
+completeRadioButton.addEventListener("change", (e) => getUserSelection(e));
+activeRadioButton.addEventListener("change", (e) => getUserSelection(e));
