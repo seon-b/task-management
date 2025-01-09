d72ab84e9f135f01a9731354fb94f58253f0c8ad
seon-b <86736916+seon-b@users.noreply.github.com>
2025-01-09 10:22:41 -0500

    Added morgan to app.js

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 const express = require("express");
+const morgan = require("morgan");
+
 const helmet = require("helmet");
 const path = require("path");
 const app = express();
+
 const PORT = process.env.PORT || 3000;
 
 app.use(helmet());
+app.use(morgan("tiny"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "../public")));
