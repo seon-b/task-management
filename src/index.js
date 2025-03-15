1226287ead28516535c7a0a7f2e7223650f8cbd8
seon-b <86736916+seon-b@users.noreply.github.com>
2025-03-15 15:00:27 -0400

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
 const express = require("express");
 const morgan = require("morgan");
-const session = require("express-session");
-const passport = require("passport");
-require("./auth-strategies/passportLocal");
-
 const helmet = require("helmet");
 const path = require("path");
-const { validateUser } = require("./auth-strategies/passportLocal");
+
 const app = express();
 
 const PORT = process.env.PORT || 3000;
@@ -16,38 +12,12 @@ app.use(morgan("tiny"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "../public")));
-app.use(
-  session({
-    secret: process.env.SESSION_SECRET,
-    saveUninitialized: false,
-    resave: false,
-    cookie: {
-      maxAge: 60000 * 120,
-    },
-  })
-);
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.use("/api", require("./routes"));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "../views"));
 
 app.get("/", (req, res, next) => {
   res.render("index", { message: null });
-});
-
-app.get("/dashboard", validateUser, (req, res, next) => {
-  res.render("dashboard", { profileName: req.user.email, message: null });
-});
-
-app.get("/login", (req, res, next) => {
-  res.render("login", { message: null });
-});
-
-app.get("/login-failed", (req, res, next) => {
-  res.render("login", { message: "Login Unsuccessful" });
 });
 
 app.listen(PORT, () => {
